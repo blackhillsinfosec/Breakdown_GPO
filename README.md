3dfc1938ad53ad7d6e69dfe2548483a61ba9d04c
Kiersten Gross <kiersten@blackhillsinfosec.com>
2023-02-24 21:16:44 -0700

    Version 2.0.0

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # breakdown_gpo
-#### Version 1.2.1
+
+#### Version 2.0.0
 
 During Active Directory Reviews, it is beneficial to review all of the GPOs.
 On a domain controller, the command `Get-GPOReport -All -Domain "domain.com" -Server "ACME-DC1" -ReportType HTML -Path "C:\GPOReport.html"` will export all of the GPOs for `domain.com`. However, it will combine all of the HTML reports into a single file; having several HTML files inside a single file causes problems and cannot be read easily with a browser. This tool breaks down the large file into a collection of individual HTML files that can be opened in a browser.
@@ -7,25 +8,23 @@ On a domain controller, the command `Get-GPOReport -All -Domain "domain.com" -Se
 By default, this script will create an output directory ("gpo_out") in the current working directory.
 The script will ask if the output directory needs to be a different name; leave blank for default.
 
-A summary of the broken down GPOs will be created inside the new output directory called "0_gpo_summary.txt".
-GPOs that are not linked in the domain will be combined into a directory named "0_unlinked_gpos" inside the new output directory.
+A summary of the broken down GPOs will be created inside the new output directory called "gpo_summary.txt".
+GPOs will be organized according to their status: "effective", "computer", "user", "disabled", or "ineffective".
+GPOs are considered "ineffective" if they have a security filter of "none".
 
-GPOs that are linked will be prepended with a number to preserve the order they are listed in the original GPOReport.
+GPOs that are effective ("effective", "computer", "user") will be prepended with a number to preserve the order they are listed in the original GPOReport.
 Using this ordering, it should be possible to calculate winning GPOs.
 
+#### Pre-Reqs
+`python3 -m pip install colorama`
+
 #### Version Notes
-##### 1.2.0
-###### Introduced threading to the read and parse process to make it faster.
-Four threads are used for multi-threading. This setting is not changeable in this version.
+##### 2.0.0
+###### Major Refactor
+Separated major code aspects into modules.
 
-###### Introduced ability to use nested directories as an output destination.
-
-##### 1.2.1
-###### Correct Encoding types for generated HTML files.
-This is to fix encoding issues that may be created by special Unicode characters that do not belong in a normal file.
-
-###### Add Exception strings to except blocks.
-Generate better error output.
+###### Distinguish each GPO by status
+Determine if the GPO is "Enabled", "Disabled", "Computer", "User", or "Ineffective".
 
 #### Syntax
 
---
+++ modules/gpo_threading.py
@@ -0,0 +1,99 @@
+from colorama import Fore, Style
+import threading, sys, time, modules.gpo_indexes as gpo_indexes
+
+# Class for threads to index file
+class indx_thread(threading.Thread):
+    def __init__(self, read_material, read_start, read_end, thread_name):
+        threading.Thread.__init__(self)
+        #Each thread will track it's own found indices and data
+        self.name = thread_name
+        self.read_material = read_material
+        self.read_start = read_start
+        self.read_end = read_end
+        self.html_start = []
+        self.html_end = []
+        self.title_start = []
+        self.title_end = []
+        self.status_anchor = []
+        self.effective_anchor = []
+    def __str__(self):
+        return f'''
+            html_start indexes are {self.html_start}
+            html_end indexes are {self.html_end}
+            title_start indexes are {self.title_start}
+            title_end indexes are {self.title_end}
+            status_anchor indexes are {self.status_anchor}
+            effective_anchor indexes are {self.effective_anchor}
+        '''
+    def run(self):       
+        for indx, content in enumerate(self.read_material[self.read_start:self.read_end]):
+            if(content=="<"):
+                # Search for opening HTML tags of interest
+                if(self.read_material[(self.read_start+indx):(self.read_start+indx)+6]=="<html " and self.read_material[(self.read_start+indx)-1:(self.read_start+indx)+6]!="\"<html "):
+                    self.html_start.append((self.read_start+indx))
+                elif(self.read_material[(self.read_start+indx):(self.read_start+indx)+7]=="<title>" and self.read_material[(self.read_start+indx)-1:(self.read_start+indx)+8]!="\"<title>\""):
+                    self.title_start.append((self.read_start+indx)+7)
+                # Determine the status of each GPO
+                elif(self.read_material[(self.read_start+indx):(self.read_start+indx)+56]=="<tr><td scope=\"row\">GPO Status</td><td>Enabled</td></tr>"):
+                    self.status_anchor.append("enabled    ")
+                elif(self.read_material[(self.read_start+indx):(self.read_start+indx)+70]=="<tr><td scope=\"row\">GPO Status</td><td>All settings disabled</td></tr>"):
+                    self.status_anchor.append("disabled   ")
+                elif(self.read_material[(self.read_start+indx):(self.read_start+indx)+71]=="<tr><td scope=\"row\">GPO Status</td><td>User settings disabled</td></tr>"):
+                    self.status_anchor.append("computer   ")
+                elif(self.read_material[(self.read_start+indx):(self.read_start+indx)+75]=="<tr><td scope=\"row\">GPO Status</td><td>Computer settings disabled</td></tr>"):
+                    self.status_anchor.append("user       ")
+                elif(self.read_material[(self.read_start+indx):(self.read_start+indx)+35]=="<tr><td scope=\"row\">GPO Status</td>"):
+                    self.status_anchor.append((self.read_start+indx))
+                # Determine if the GPO is ineffective
+                elif(self.read_material[(self.read_start+indx):(self.read_start+indx)+235]=="<b>The settings in this GPO can only apply to the following groups, users, and computers:</b></div>\n<div class=\"he4i\">\n<table class=\"info\" cellpadding=\"0\" cellspacing=\"0\"><tr><th scope=\"col\">Name</th></tr><tr><td>None</td></tr></table>"):
+                    self.effective_anchor.append("ineffective")
+                elif(self.read_material[(self.read_start+indx):(self.read_start+indx)+213]=="<b>The settings in this GPO can only apply to the following groups, users, and computers:</b></div>\n<div class=\"he4i\">\n<table class=\"info\" cellpadding=\"0\" cellspacing=\"0\"><tr><th scope=\"col\">Name</th></tr><tr><td>"):
+                    self.effective_anchor.append((self.read_start+indx))
+                # Search for closing HTML tags of interest.
+                elif(self.read_material[(self.read_start+indx):(self.read_start+indx)+7]=="</html>" and self.read_material[(self.read_start+indx):(self.read_start+indx)+8]!="</html>\""):
+                    self.html_end.append((self.read_start+indx))
+                elif(self.read_material[(self.read_start+indx):(self.read_start+indx)+8]=="</title>" and self.read_material[(self.read_start+indx)-1:(self.read_start+indx)+9]!="\"</title>\"" and self.read_material[(self.read_start+indx-1):(self.read_start+indx)+9]!="\"</title>\\"):
+                    self.title_end.append((self.read_start+indx))
+
+# Index GPOReport
+def concurrent_index_file(filecontent):
+    thread_count = 3
+    thread_divisible = int((len(filecontent)/thread_count))
+    thread_pool = []
+
+    for count in range(thread_count):
+        if(count!=thread_count-1):
+            thread_pool.append(indx_thread(filecontent, (thread_divisible*count), ((thread_divisible*(count+1))-1), "thread"+str(count)))
+        else:
+            thread_pool.append(indx_thread(filecontent, (thread_divisible*count), len(filecontent), "thread"+str(count)))
+    
+    # Push threads to the background and start
+    for thread in thread_pool:
+        thread.setDaemon(True)
+        thread.start()
+    
+    # Print feedback to indicate processing.
+    counter = 0
+    options = [
+        "\r"+Style.BRIGHT+Fore.BLUE+"."+"   "+Style.RESET_ALL,
+        "\r"+Style.BRIGHT+Fore.BLUE+"."+Fore.GREEN+".  "+Style.RESET_ALL,
+        "\r"+Style.BRIGHT+Fore.BLUE+"."+Fore.GREEN+"."+Fore.RED+". "+Style.RESET_ALL,
+        "\r"+Style.BRIGHT+Fore.BLUE+"."+Fore.GREEN+"."+Fore.RED+"."+Fore.WHITE+"."+Style.RESET_ALL]
+    while(threading.active_count() > 1):
+        if(counter==4):
+            counter=0
+        print(options[counter], end="")
+        time.sleep(0.05)
+        counter+=1
+
+    Indexes = gpo_indexes.GPO_Indexes()
+    Indexes.construct_master_index(thread_pool)
+    return Indexes
+
+# Check for Errors - Exit in the case of error
+def review_validation(validate, start):
+    if(validate):
+        print("\r"+validate)
+        sys.exit()
+    else:
+        print(Style.BRIGHT+Fore.GREEN+f'\rParsing the GPO Report took{time.time() - start : .2f} seconds.'+Style.RESET_ALL)
